8798fd39f240e1ada382bc6afcf5c6786a368452
fred-dev22 <73308599+fred-dev22@users.noreply.github.com>
2021-11-18 10:42:59 +0100

    minimisation

--- src/Automate/Minimiser.java
+++ src/Automate/Minimiser.java
@@ -4,6 +4,9 @@ import Utils.Constans;
 import Utils.ValidationException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
+import java.util.ArrayDeque;
+
 import Utils.Constans;
 import Utils.ValidationException;
 
@@ -18,9 +21,12 @@ public class Minimiser {
     public static Automate minimisation(Automate aut) throws ValidationException {
         // creation du nouveau automate minimiser
         Automate autMinimiser = new Automate(Constans.APHABET);
+        int test = 0;
 
         // liste des partitons apres avoir applique la theorie du congossa
         List<List<Etat>> MesPartitions = new ArrayList<List<Etat>>();
+
+        Queue<List<Etat>> nonTraiter = new ArrayDeque<List<Etat>>();
 
         // creation de la partition 1 , qui est lensemble des etats finaux de l'automate
         List<Etat> partition1 = aut.getFinalStates();
@@ -33,60 +39,79 @@ public class Minimiser {
             }
         }
 
-        // utilisons la theorie du congossa pour essayer de creer d'autre partitions a
-        // partie des partitions 1
-        // chaque etat se trouvant dans la partition 1
-        for (Etat e : partition1) {
-            // je parcours l'alphabet de lautomate sauf epsilon
-            for (String c : aut.getAlphabet()) {
-                if (!c.equals(Constans.EPSILON)) {
-                    // si la trasition de l'etat e sur un symbol de l'alphabet donne un etat qui
-                    // n'est pas dans la partition
-                    if (!(partition1.contains(aut.getTransitionTable().getTransition(e, c)))) {
-                        // je cree une nouvelle partition qui ne contient que l'etat e
-                        List<Etat> l = new ArrayList<Etat>();
-                        l.add(e);
-                        // j'ajoute cette partition a ma liste de partiton
-                        MesPartitions.add(l);
-                        // je retire cet etat de la partition 1
-                        partition1.remove(e);
+        // j'ajoute d'abord les deux partitions a la partition final
+        nonTraiter.add(partition1);
+        nonTraiter.add(partition2);
+        System.out.println("\nVoici les deux parttions de departs : ");
+        System.out.println(nonTraiter);
+        // je commencer a traiter les partitions
+        while (!nonTraiter.isEmpty()) {
+            // on travaill sur une partition s'il a plus d'un etat
+            List<Etat> partition = nonTraiter.poll();
+            // on cree une liste qui vas contenir les etats qui trahissent le congossa
+            List<Etat> l = new ArrayList<Etat>();
+            List<Etat> partitionInter = new ArrayList<Etat>(partition);
+
+            if (partition.size() > 1) {
+                // // on parcours les Etats de la partition
+                // Queue<Etat> etatsNonTraiter = new ArrayDeque<Etat>(partition);
+                for (Etat etat : partition) {
+                    // // je parcours tous les symbol de l'alphabet de l'automate sauf epsilone
+                    for (String symb : aut.getAlphabet()) {
+                        if (!symb.equals(Constans.EPSILON)) {
+                            // // si la trasition de l'etat sur un symbol de l'alphabet donne un etat qui
+                            // // n'est pas dans la partition
+
+                            if (!partition.contains(aut.getTransitionTable().getTransition(etat, symb).get(0))) {
+                                // // j'ajoute l'etat a la liste des trahisseurs de la partition
+                                if (!l.contains(etat)) {
+                                    l.add(etat);
+
+                                }
+                                partitionInter.remove(etat);
+
+                            }
+
+                        }
+
+                    }
+
+                }
+                if (partitionInter.isEmpty()) {
+                    MesPartitions.add(l);
+                } else
+                    nonTraiter.add(l);
+
+            }
+            if (partitionInter.size() > 1) {
+                for (String symb : aut.getAlphabet()) {
+                    if (!symb.equals(Constans.EPSILON)) {
+                        // // si la trasition de l'etat sur un symbol de l'alphabet donne un etat qui
+                        // // n'est pas dans la partition
+                        for (Etat etat : partitionInter) {
+                            if (!partitionInter.contains(aut.getTransitionTable().getTransition(etat, symb).get(0))) {
+                                test = 1;
+
+                            }
+                        }
+
                     }
 
                 }
             }
-
-        }
-
-        // pour la partition 2
-
-        // en faite c'est le meme code executer sur la partition 1
-        // je doit normalement creer une fonction pour eviter la repetition du code ,
-        // mais bon ....
-        for (Etat e : partition2) {
-            // je parcours l'alphabet de lautomate sauf epsilon
-            for (String c : aut.getAlphabet()) {
-                if (!c.equals(Constans.EPSILON)) {
-                    if (!(partition2.contains(aut.getTransitionTable().getTransition(e, c)))) {
-                        List<Etat> l = new ArrayList<Etat>();
-                        l.add(e);
-                        MesPartitions.add(l);
-                        partition2.remove(e);
-                    }
-
+            if (!partitionInter.isEmpty()) {
+                if (test == 0) {
+                    MesPartitions.add(partitionInter);
+                } else {
+                    nonTraiter.add(partitionInter);
+                    test = 0;
                 }
             }
 
         }
-
-        // j'ajoute les deux partitions initiale a mes partition
-        // mais ici les deux partitons ci , ne serons plus pareil qu'au depart car
-        // j'aurais enlever les etats qui sur un symbole de l'alphabet transite vers un
-        // etat ne faisant pas partir de la meme partiton ou sous groupe
-        MesPartitions.add(partition1);
-        MesPartitions.add(partition2);
 
         // j'affiche mes partition
-        System.out.println("\n info sur la minimisation");
+        System.out.println("voici les partitions creer apres la theorie du congossa :");
 
         System.out.println(MesPartitions);
 
