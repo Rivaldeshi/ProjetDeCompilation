2d0fdf94fdc1c0d01b1aec41e678a0f47c6bb3dc
RIVALDES Ranol <tiyorivaldes@gmail.com>
2021-11-23 21:12:03 +0100

    le desin est un peu bon

--- src/DrawAutomate/Draw.java
+++ src/DrawAutomate/Draw.java
@@ -17,56 +17,24 @@ import javax.swing.SwingConstants;
 import com.mxgraph.layout.hierarchical.mxHierarchicalLayout;
 import com.mxgraph.util.mxConstants;
 
-
 public class Draw {
 
 	private static final mxStylesheet STYLE = new mxStylesheet();
 
 	static {
 		HashMap<String, Object> style = new HashMap<String, Object>();
-		style.put(mxConstants.STYLE_SHAPE, mxConstants.SHAPE_TRIANGLE);
-		style.put(mxConstants.STYLE_FONTCOLOR, "#000000");
-		style.put(mxConstants.STYLE_PERIMETER, mxPerimeter.EllipsePerimeter);
-		style.put(mxConstants.STYLE_VERTICAL_ALIGN, mxConstants.ALIGN_BOTTOM);
-		style.put(mxConstants.STYLE_ALIGN, mxConstants.ALIGN_CENTER);
-		style.put(mxConstants.STYLE_FILLCOLOR, "#FFFFFF");
-		style.put(mxConstants.STYLE_STROKECOLOR, "#E3A646");
-		STYLE.putCellStyle("PINK_INIT", style);
-
-		style = new HashMap<String, Object>();
-		style.put(mxConstants.STYLE_SHAPE, mxConstants.SHAPE_RECTANGLE);
-		style.put(mxConstants.STYLE_FONTCOLOR, "#000000");
-		style.put(mxConstants.STYLE_PERIMETER, mxPerimeter.EllipsePerimeter);
-		style.put(mxConstants.STYLE_VERTICAL_ALIGN, mxConstants.ALIGN_BOTTOM);
-		style.put(mxConstants.STYLE_ALIGN, mxConstants.ALIGN_CENTER);
-		style.put(mxConstants.STYLE_FILLCOLOR, "#E34646");
-		style.put(mxConstants.STYLE_STROKECOLOR, "#000000");
-		STYLE.putCellStyle("PINK_FINAL", style);
-
-		style = new HashMap<String, Object>();
-		style.put(mxConstants.STYLE_SHAPE, mxConstants.SHAPE_DOUBLE_ELLIPSE);
-		style.put(mxConstants.STYLE_FONTCOLOR, "#000000");
-		style.put(mxConstants.STYLE_PERIMETER, mxPerimeter.EllipsePerimeter);
-		style.put(mxConstants.STYLE_VERTICAL_ALIGN, mxConstants.ALIGN_BOTTOM);
-		style.put(mxConstants.STYLE_ALIGN, mxConstants.ALIGN_CENTER);
-		style.put(mxConstants.STYLE_FILLCOLOR, "#E34646");
-		style.put(mxConstants.STYLE_STROKECOLOR, "#E3A646");
-		STYLE.putCellStyle("PINK_BOTH", style);
-
 		style = new HashMap<String, Object>();
 		style.put(mxConstants.STYLE_SHAPE, mxConstants.SHAPE_ELLIPSE);
 		style.put(mxConstants.STYLE_FONTCOLOR, "#000000");
 		style.put(mxConstants.STYLE_PERIMETER, mxPerimeter.EllipsePerimeter);
 		style.put(mxConstants.STYLE_ALIGN, mxConstants.ALIGN_CENTER);
 		style.put(mxConstants.STYLE_FILLCOLOR, "#FFFFFF");
-		style.put(mxConstants.STYLE_STROKECOLOR, "#000000");
 		STYLE.putCellStyle("PINK_NONE", style);
 
 		style = new HashMap<String, Object>();
 		style.put(mxConstants.STYLE_SHAPE, mxConstants.SHAPE_CONNECTOR);
 		style.put(mxConstants.STYLE_ENDARROW, mxConstants.ARROW_CLASSIC);
 		style.put(mxConstants.STYLE_VERTICAL_ALIGN, mxConstants.ALIGN_LEFT);
-		
 		style.put(mxConstants.STYLE_ALIGN, mxConstants.ALIGN_CENTER);
 		style.put(mxConstants.STYLE_STROKECOLOR, "black");
 		style.put(mxConstants.STYLE_FONTCOLOR, "#000000");
@@ -75,7 +43,7 @@ public class Draw {
 
 	public static Panel drawAutomate(Automate automate)
 			throws ValidationException {
-	
+
 		// Creates graph with model
 		mxGraph graph = new mxGraph();
 		graph.setStylesheet(STYLE);
@@ -88,7 +56,8 @@ public class Draw {
 
 			for (Etat et : automate.getStates()) {
 
-				Object v1 = graph.insertVertex(parent, null, et, 0, 0, 50,50, "PINK_NONE");
+				Object v1 = graph.insertVertex(parent, null, et, 0, 0, 50, 50,
+						"PINK_NONE");
 
 				vertexs.add(v1);
 
@@ -115,7 +84,6 @@ public class Draw {
 		mxHierarchicalLayout layout = new mxHierarchicalLayout(graph);
 
 		layout.setOrientation(SwingConstants.WEST);
-		
 
 		layout.execute(parent);
 
